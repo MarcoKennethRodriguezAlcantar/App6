584ff0af649994ab84e27a918156c316f5eb75bd
Marco <Marco@LAPTOP-JRE178RN>
2021-10-03 15:57:59 -0500

    Agregar archivos de proyecto.

---
+++ App6/App6/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,73 @@
+﻿using App6.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App6.ViewModels
+{
+    class MainPageViewModel : BaseViewModel 
+    {
+        private string _nombreUsuario;
+        private string _password;
+        private bool _isErrorLogin;
+        private Command _loginCommand;
+    
+
+    public MainPageViewModel(INavigation navigation) : base(navigation)
+    {
+
+    }
+
+    public string Nombre
+    {
+        get => _nombreUsuario;
+        set
+        {
+            _nombreUsuario = value;
+            OnPropertyChanged();
+
+
+        }
+    }
+
+    public string Contrasena
+    {
+        get => _password;
+        set
+        {
+            _password = value;
+            OnPropertyChanged();
+
+
+        }
+    }
+
+    public Command LoginCommand
+    {
+        get => _loginCommand ?? (_loginCommand = new Command(GoToViewTwoAction));
+    }
+
+    public bool IsErrorLogin
+    {
+        get => _isErrorLogin;
+        set
+        {
+            _isErrorLogin = value;
+
+            OnPropertyChanged();
+        }
+    }
+
+    private void GoToViewTwoAction()
+    {
+        IsErrorLogin = !(Nombre == "Marco" && Contrasena == "Hola");
+        if (!IsErrorLogin)
+        {
+            Nombre = String.Empty;
+            Contrasena = String.Empty;
+            Navigation.PushAsync(new PaginaInicio());
+        }
+    }
+    }
+}
---
+++ App6/App6/ViewModels/PagigaInicioViewModel.cs
@@ -0,0 +1,33 @@
+﻿using App6.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App6.ViewModels
+{
+    public class PagigaInicioViewModel : BaseViewModel
+    {
+        Command _goToDetalles;
+        Command _logOut;
+
+        public PagigaInicioViewModel(INavigation navigation = null) : base(navigation)
+        {
+
+        }
+
+        public Command GoToDetallesCommand
+        {
+            get => _goToDetalles ?? (_goToDetalles = new Command(GoToDetallesAction));
+        }
+
+        public Command LogOutCommand
+        {
+            get => _logOut ?? (_logOut = new Command(() => Navigation.PopToRootAsync()));
+        }
+        private void GoToDetallesAction(object obj)
+        {
+            Navigation.PushAsync(new PaginaDetalles());
+        }
+    }
+}
